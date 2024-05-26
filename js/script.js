9281b998d9b2565f4c03cfdb9e077f1f61d19ae1
CodeKiro <kirofiniello@outlook.it>
2024-05-27 01:24:21 +0200

    ended up

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const play = document.getElementById('bottone');
 function newGame(numberOfCells, containerElement) {
     containerElement.innerHTML = '';  // Azzera il contenuto precedente della griglia
 
+    const bombPositions =getUniqueBombs (16, 1, 100);
+
     for (let index = 0; index < numberOfCells; index++) {
         const squareElement = document.createElement('article');
         squareElement.classList.add('square');
@@ -13,7 +15,14 @@ function newGame(numberOfCells, containerElement) {
         contentEL.append(index + 1);
         squareElement.appendChild(contentEL);
 
-        squareElement.addEventListener('click', function() {
+        squareElement.addEventListener('click', function(){
+
+            if(bombPositions.includes(index + 1)){
+                squareElement.classList.add('bomb');
+                alert('LOSE');
+                containerElement.innerHTML = '';
+                return;
+            }
             if ((index + 1) % 2 == 0) {
                 squareElement.classList.add('active');
             } else {
@@ -29,24 +38,17 @@ function getRandomInt(min, max){
     return Math.floor( Math.random() * ((max + 1) - min )) + min;
 }
 
-function getUniqueRandomInt(uninvitedNumbers, min , max){
+function getUniqueBombs (numberOfBombs, min , max){
 
-    if ( max - min > uninvitedNumbers.length){
-        return false;
+    const bombPositions = new Set();
+
+    while (bombPositions.size < numberOfBombs){
+        const randomNumber = getRandomInt(min, max);
+        bombPositions.add(randomNumber);
     }
 
-    let randomNumber;
-    let isFound = false;
-
-    while(!isFound){
-            randomNumber = getRandomInt(min, max);
-        if( uninvitedNumbers.includes(raandomNumber) !== false){
-            isFound = true;
-        }
-    }
-    return randomNumber;
+    return Array.from(bombPositions);
 }
-
 play.addEventListener('click', function() {
         newGame(100, gridElement);
 
